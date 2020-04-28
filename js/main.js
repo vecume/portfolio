52f14232b0607ba4b65a42d1935140160423c1b9
vecume <shahzod.bek200109@gmail.com>
2020-04-28 23:39:21 +0500

    Remove bg slide show

--- js/main.js
+++ js/main.js
@@ -4,28 +4,7 @@ var dir = loc.substring(0, loc.lastIndexOf('/'));
 
 const elLogo = document.querySelector('.logo')
 const elLayer = document.querySelector('.layer');
-elsHexagons.forEach(hex => {
-  hex.addEventListener('mouseover', (evt) => {
-    if (elLayer.dataset.imgId != evt.target.dataset.imgId) {
-      elLayer.animate([{
-          transform: 'scale(1)'
-        },
-        {
-          transform: 'scale(1.5)'
-        }
-      ], {
-        duration: 15000,
-        iterations: 1,
-        fill: 'forwards',
-        easing: 'ease'
-      })
-      elLayer.style = `background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('../PortFolio/img/${evt.target.dataset.imgId}.jpg');`;
-      elLayer.dataset.imgId = evt.target.dataset.imgId;
 
-    }
-
-  });
-});
 
 
 const elCursor = document.querySelector('.cursor');
