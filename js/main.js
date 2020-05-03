14a9d8ee0f0e7e50f41d16dc4853bf59006ae460
vecume <shahzod.bek200109@gmail.com>
2020-05-03 14:55:40 +0500

    Add another page transition

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-import Highway from '@dogstudio/highway';
-import Fade from './transitions';
-
-
-const H = new Highway.Core({
-  transitions: {
-    default: Fade
-  }
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
 });
-
-
-
-const elsHexagons = document.querySelectorAll('.hex');
-var loc = window.location.pathname;
-var dir = loc.substring(0, loc.lastIndexOf('/'));
-
-const elLogo = document.querySelector('.logo')
-const elLayer = document.querySelector('.layer');
-
-
-
 const elCursor = document.querySelector('.cursor');
 
+const elAbout = document.querySelector('.about-main');
+
+// try {
+//   elAbout.parentElement.style.overflowY = 'scroll';
+// } catch (error) {
+//   console.log(error);
+// }
+
 const elsLink = document.querySelectorAll('a');
-const elsNavItem = document.querySelectorAll('.menu__item')
 window.addEventListener('mousemove', (e) => {
   elCursor.style.top = e.pageY + 'px';
   elCursor.style.left = e.pageX + 'px';
