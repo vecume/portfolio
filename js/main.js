af209736fc4d885e773c8c4e6d954a33e0894e14
vecume <shahzod.bek200109@gmail.com>
2020-05-04 13:51:31 +0500

    Complete ABout page

--- js/main.js
+++ js/main.js
@@ -1,39 +1,83 @@
+function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
+AOS.init();
+
+const elAbout = document.querySelector('.about-main');
+const elVideo = document.querySelector('#myVideo')
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.set('.loading-screen', { transformOrigin: "bottom left"});
+  tl.to('.loading-screen', { duration: .5, scaleY: 1});
+  tl.to('.loading-screen', { duration: .5, scaleY: 0, skewX: 0, transformOrigin: "top left", ease: "power1.out", delay: 0.5});
+}
+
+// Function to animate the content of each page
+function contentAnimation() {
+  var tl = gsap.timeline();
+  
+  tl.from('ul.social__menu li', { duration: 0.4, opacity: 0, translateX: 10,  delay:0.7, stagger: 0.1});
+  tl.from('.logo', { duration: 0.3, opacity: 0,  translateY: -10});
+  tl.from('ul#hexGrid li', { duration: 0.5, opacity: 0, scale: 0, stagger: 0.1});
+
+}
+
 barba.init({
+  sync:true,
+
   transitions: [{
-    name: 'opacity-transition',
-    leave(data) {
-      return gsap.to(data.current.container, {
-        opacity: 0
-      });
+  
+    async leave(data) {
+      const done = this.async();
+      pageTransition();
+      await delay(500);
+      done();
+      try {
+        document.body.style.overflowY = 'scroll';
+      } catch (error) {
+        console.log(error);
+      }
     },
-    enter(data) {
-      return gsap.from(data.next.container, {
-        opacity: 0
-      });
+
+    async enter(data) {
+      contentAnimation();
+    },
+
+    async once(data) {
+      contentAnimation();
     }
+
   }]
 });
 const elCursor = document.querySelector('.cursor');
 
-const elAbout = document.querySelector('.about-main');
 
-// try {
-//   elAbout.parentElement.style.overflowY = 'scroll';
-// } catch (error) {
-//   console.log(error);
-// }
+
+
 
 const elsLink = document.querySelectorAll('a');
 window.addEventListener('mousemove', (e) => {
-  elCursor.style.top = e.pageY + 'px';
-  elCursor.style.left = e.pageX + 'px';
+  if (window.innerWidth > 600) {
+    elCursor.style.top = e.pageY + 'px';
+    elCursor.style.left = e.pageX + 'px';
+  }
 });
 
 elsLink.forEach(link => {
-  link.addEventListener('mouseover', (evt) => {
-    elCursor.classList.add('link-grow');
-  });
-  link.addEventListener('mouseleave', () => {
-    elCursor.classList.remove('link-grow');
-  });
+  if (window.innerWidth > 600) {
+    link.addEventListener('mouseover', () => {
+      elCursor.classList.add('link-grow');
+      console.log('in');
+    });
+    link.addEventListener('mouseleave', () => {
+      elCursor.classList.remove('link-grow');
+      console.log('out');
+    });
+  }
 });
