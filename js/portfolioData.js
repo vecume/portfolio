b122b11c2ec55f887a1b95f6c31cdef8b9b89dbd
vecume <shahzod.bek200109@gmail.com>
2020-05-05 02:26:30 +0500

    Create portfolio page

---
+++ js/portfolioData.js
@@ -0,0 +1,74 @@
+const projects = [
+  {
+    type: "landing",
+    link: "./fylo",
+    name: "Fylo",
+    img: "fylo.png"
+  },
+  {
+    type: "landing",
+    link: "./digital-agency",
+    name: "Digital Agency",
+    img: "digitalagency.png"
+  },
+  {
+    type: "landing",
+    link: "./minimal-yelessandri",
+    name: "Minimal Yelessandri",
+    img: "minimal.png"
+  },
+  {
+    type: "landing",
+    link: "./applab",
+    name: "AppLab",
+    img: "applab.jpg"
+  },
+  {
+    type: "api",
+    link: "./pokedex",
+    name: "PokeDex",
+    img: "pokedex.png"
+  },
+  {
+    type: "api",
+    link: "./music-app",
+    name: "Music App",
+    img: "musicapp.jpg"
+  },
+  {
+    type: "api",
+    link: "./movie-app",
+    name: "Movie App",
+    img: "movieapp.png"
+  },
+  {
+    type: "channel",
+    link: "https://t.me/joinchat/AAAAAEsteANyfWvQJigWSg",
+    name: "BILASIZMI?",
+    img: "bilasizmi.jpg"
+  },
+  {
+    type: "channel",
+    link: "https://t.me/joinchat/AAAAAE7hrgjQ0IFCNwRlkg",
+    name: "QORA TARIX",
+    img: "tarix.jpg"
+  },
+  {
+    type: "channel",
+    link: "https://t.me/joinchat/AAAAAEv-RMNVjwg4xhvtnA",
+    name: "Eh, Odamlar",
+    img: "ehodam.jpg"
+  },
+  {
+    type: "bots",
+    link: "https://t.me/coronaWorldInfo_bot",
+    name: "Corona Virus Stats",
+    img: "corona.jpg"
+  },
+  {
+    type: "bots",
+    link: "https://t.me/namozVaqti_Robot",
+    name: "NAMOZ VAQTI",
+    img: "namoz.jpg"
+  },
+]
