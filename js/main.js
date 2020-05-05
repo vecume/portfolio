4afd533e7daae3898ba91185c946b55fdc4460d8
vecume <shahzod.bek200109@gmail.com>
2020-05-05 21:53:55 +0500

    Solve problems with DOM changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-
-
-
 const elCursor = document.querySelector('.cursor');
 const elAbout = document.querySelector('.about-main');
 const elVideo = document.querySelector('#myVideo')
+
 function delay(n) {
   n = n || 2000;
   return new Promise(done => {
@@ -13,28 +11,69 @@ function delay(n) {
   });
 }
 
+function hoverEffect() {
+  elCursor.classList.remove('link-grow');
+  var elsLink = document.querySelectorAll('a');
+  elsLink.forEach(link => {
+    if (window.innerWidth > 600) {
+      link.addEventListener('mouseover', () => {
+        elCursor.classList.add('link-grow');
+      });
+      link.addEventListener('mouseleave', () => {
+        elCursor.classList.remove('link-grow');
+      });
+    }
+  });
+}
 
 function pageTransition() {
   var tl = gsap.timeline();
-  tl.set('.loading-screen', { transformOrigin: "bottom left"});
-  tl.to('.loading-screen', { duration: .5, scaleY: 1});
-  tl.to('.loading-screen', { duration: .5, scaleY: 0, skewX: 0, transformOrigin: "top left", ease: "power1.out", delay: 0.5});
+  tl.set('.loading-screen', {
+    transformOrigin: "bottom left"
+  });
+  tl.to('.loading-screen', {
+    duration: .5,
+    scaleY: 1
+  });
+  tl.to('.loading-screen', {
+    duration: .5,
+    scaleY: 0,
+    skewX: 0,
+    transformOrigin: "top left",
+    ease: "power1.out",
+    delay: 0.5
+  });
 }
 
 // Function to animate the content of each page
 function contentAnimation() {
   var tl = gsap.timeline();
-  tl.from('ul.social__menu li', { duration: 0.4, opacity: 0, translateX: 10,  delay:0.7, stagger: 0.1});
-  tl.from('.logo', { duration: 0.3, opacity: 0,  translateY: -10});
-  tl.from('ul#hexGrid li', { duration: 0.5, opacity: 0, scale: 0, stagger: 0.1});
+  tl.from('ul.social__menu li', {
+    duration: 0.4,
+    opacity: 0,
+    translateX: 10,
+    delay: 0.7,
+    stagger: 0.1
+  });
+  tl.from('.logo', {
+    duration: 0.3,
+    opacity: 0,
+    translateY: -10
+  });
+  tl.from('ul#hexGrid li', {
+    duration: 0.5,
+    opacity: 0,
+    scale: 0,
+    stagger: 0.1
+  });
 
 }
 
 barba.init({
-  sync:true,
+  sync: true,
 
   transitions: [{
-  
+
     async leave(data) {
       const done = this.async();
       pageTransition();
@@ -53,13 +92,29 @@ barba.init({
 
     async once(data) {
       contentAnimation();
-      
+
     }
 
   }]
 });
 
 
+
+const observer = new MutationObserver(mutation => {
+  if (document.body.children[3].classList.value === "portfolio-main") {
+    portfolio();
+  }
+  hoverEffect();
+});
+
+//LISTEN BODY CAHNGES
+
+observer.observe(document.body, {
+  childList: true,
+  attributes: false,
+  subtree: false,
+  characterData: false
+});
 
 
 
@@ -68,22 +123,14 @@ window.addEventListener('mousemove', (e) => {
     elCursor.style.top = e.pageY + 'px';
     elCursor.style.left = e.pageX + 'px';
   }
+
+
 });
 
 
 
-setInterval(() => {
-  var elsLink = document.querySelectorAll('a');
-  elsLink.forEach(link => {
-    if (window.innerWidth > 600) {
-      link.addEventListener('mouseover', () => {
-        elCursor.classList.add('link-grow');
-        console.log('in');
-      });
-      link.addEventListener('mouseleave', () => {
-        elCursor.classList.remove('link-grow');
-        console.log('out');
-      });
-    }
-  });
-},3000);
+const elPortfolio = document.querySelector('.portfolio-link');
+
+document.body.addEventListener('change', () => {
+  console.log('ok');
+});
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,47 +1,53 @@
-const elsSortButton = document.querySelectorAll('.sort-btn');
-const elResultsBox = document.querySelector('.sort-results');
-const elResultTemplate = document.querySelector('#sortedPostTemplate').content;
+function portfolio() {
+  const elsSortButton = document.querySelectorAll('.sort-btn');
+  const elResultsBox = document.querySelector('.sort-results');
+  const elResultTemplate = document.querySelector('#sortedPostTemplate').content;
 
 
-showResults(projects);
-elsSortButton.forEach(btn => {
+  showResults(projects);
+  elsSortButton.forEach(btn => {
 
-  btn.addEventListener('click', (evt) => {
-    elsSortButton.forEach(b => {
-      b.classList.remove('btn--active');
+    btn.addEventListener('click', (evt) => {
+      elsSortButton.forEach(b => {
+        b.classList.remove('btn--active');
+      });
+      evt.target.classList.add('btn--active')
+      if (evt.target.dataset.sort === "*") {
+        showResults(projects);
+      } else {
+        const results = projects.filter(p => p.type === evt.target.dataset.sort);
+        showResults(results);
+      }
     });
-    evt.target.classList.add('btn--active')
-    if(evt.target.dataset.sort === "*") {
-      showResults(projects);
-    } else {
-      const results = projects.filter(p => p.type === evt.target.dataset.sort);
-      showResults(results);
+  });
+
+  function showResults(results) {
+    elResultsBox.innerHTML = '';
+    const projectsFragment = document.createDocumentFragment();
+
+    results.forEach(r => {
+      const projectsClone = document.importNode(elResultTemplate, true);
+      projectsClone.querySelector('.project-link').href = r.link;
+      projectsClone.querySelector('.project-img').src = `img/${r.img}`;
+      projectsClone.querySelector('.project-img').alt = r.name;
+      projectsClone.querySelector('.project-title').textContent = r.name;
+      projectsClone.querySelector('.project-type').textContent = r.type.toUpperCase();
+      projectsFragment.appendChild(projectsClone);
+    });
+    elResultsBox.appendChild(projectsFragment);
+    animateResults(elResultsBox)
+  }
+
+  function animateResults(results) {
+    const elsProject = results.children;
+    for (let i = 0; i < elsProject.length; i++) {
+      setTimeout(() => {
+        try {
+          elsProject[i].classList.add('animateProject');
+        } catch (err) {
+          console.log(err);
+        }
+      }, (i + 1) * 100)
     }
-  });
-});
-
-function showResults(results) {
-  elResultsBox.innerHTML = '';
-  const projectsFragment = document.createDocumentFragment();
-  
-  results.forEach(r => { 
-    const projectsClone = document.importNode(elResultTemplate, true);
-    projectsClone.querySelector('.project-link').href = r.link;
-    projectsClone.querySelector('.project-img').src = `img/${r.img}`;
-    projectsClone.querySelector('.project-img').alt = r.name;
-    projectsClone.querySelector('.project-title').textContent = r.name;
-    projectsClone.querySelector('.project-type').textContent = r.type.toUpperCase();
-    projectsFragment.appendChild(projectsClone);
-  });
-  elResultsBox.appendChild(projectsFragment);
-  animateResults(elResultsBox)
-}
-
-function animateResults(results) {
-  const elsProject = results.children;
-  for (let i = 0; i < elsProject.length; i++) {
-    setTimeout(() => {
-      elsProject[i].classList.add('animateProject');
-    },(i+1)*100)
   }
 }
